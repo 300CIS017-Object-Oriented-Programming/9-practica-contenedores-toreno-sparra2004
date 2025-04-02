4e2404c62a95c7e2b4aa39c97c803e5c35f63d60
sparra2004 <sparra2004@javerianacali.edu.co>
2025-04-02 09:51:11 -0500

    falta terminar el main

--- Juego/src/main.cpp
+++ Juego/src/main.cpp
@@ -15,19 +15,22 @@ void menu(Torneo &torneo) {
                 "5. Salir\n";
         cout << "Opción: ";
         cin >> opc;
-        cin.ignore(); // Evita problemas con getline después de cin >>
+        cin.ignore();
 
         switch (opc) {
             case 1: {
                 cout << "\n=== Registrar videojuegos ===\n";
-                string nombreJuego;
-                int code;
+                string nombreJuego, tipo;
+                int code, difficulty;
                 cout << "Ingrese el nombre del videojuego: ";
                 getline(cin, nombreJuego);
                 cout << "Ingrese el codigo: ";
                 cin >> code;
-
-                torneo.registrarVideojuego(nombreJuego);
+                cout << "Ingrese el genero del juego: ";
+                getline(cin, tipo);
+                cout << "Ingrese la dificulta (1-5): ";
+                cin >> difficulty;
+                torneo.registrarVideojuego(code, nombreJuego, tipo, difficulty);
                 break;
             }
             case 2: {
@@ -70,7 +73,7 @@ void menu(Torneo &torneo) {
 }
 
 int main() {
-    Torneo torneo; // Se crea una instancia de Torneo
-    menu(torneo);  // Se pasa la referencia al menú
+    Torneo torneo;
+    menu(torneo);
     return 0;
 }
